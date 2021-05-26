a5c1a582a4b65045185bf41bbd33da9ea145a249
Glenn LeBlanc <g.leblanc135@gmail.com>
2021-05-25 23:42:54 -0700

    Update grid size

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ function generatePointCloudGeometry( color, width, height) {
 
     for (let i = 0; i < width; i++) {
         for (let j = 0; j < height; j++) {
-            const u = i / 20;
-            const v = j / 30;;
+            const u = i;
+            const v = j;
             const x = u - 0.5;
             //const y = (Math.cos(u * Math.PI * 4) + Math.sin( v * Math.PI * 8)) / 20;
             const y = 0;
