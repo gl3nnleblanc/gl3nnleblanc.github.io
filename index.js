c9466fab98be21728ab7f8e5cd27b012f2740ac4
Glenn LeBlanc <g.leblanc135@gmail.com>
2021-05-26 01:04:41 -0700

    Use buffers more correctly

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ import Stats from 'https://mrdoob.github.io/stats.js/build/stats.module.js';
 let renderer, scene, camera, clock, stats;
 let pointer = new THREE.Vector2();
 
-let geometry;
 
 let t;
+
+let points;
 
 
 let width = 150;
@@ -18,7 +19,6 @@ const pointSize = 0.02;
 function generatePointCloudGeometry( color, width, height) {
     const geometry = new THREE.BufferGeometry();
     const numPoints = width * height;
-
     const positions = new Float32Array( numPoints * 3 );
     const colors = new Float32Array( numPoints * 3 );
 
@@ -34,8 +34,9 @@ function generatePointCloudGeometry( color, width, height) {
             const a = u - pointer.x;
             const b = v - pointer.y;
 
-            const y = (Math.exp(-a*a - b * b)) * (Math.pow(a, 3) - 3 * a) * (Math.pow(b, 4) - 6 * Math.pow(b, 2) + 3) * Math.sin(a - t) * Math.cos(b - t) / 20;
 
+            const y = 0;
+            //const y = (Math.exp(-a*a - b * b)) * (Math.pow(a, 3) - 3 * a) * (Math.pow(b, 4) - 6 * Math.pow(b, 2) + 3) * Math.sin(a - t) * Math.cos(b - t) / 20;
             positions[3 * k] = x;
             positions[3 * k + 1] = y;
             positions[3 * k + 2] = z;
@@ -58,7 +59,7 @@ function generatePointCloudGeometry( color, width, height) {
 
 
 function generatePointCloud(color, width, height, t) {
-    geometry = generatePointCloudGeometry(color, width, height, t);
+    const geometry = generatePointCloudGeometry(color, width, height, t);
     const material = new THREE.PointsMaterial({size: pointSize, vertexColors: true});
 
     return new THREE.Points(geometry, material);
@@ -88,6 +89,38 @@ function init() {
   camera.updateMatrix();
 
   document.addEventListener('pointermove', onPointerMove);
+  points = generatePointCloud( new THREE.Color(r, g, b), width, height, t);
+  points.scale.set(5, 10, 10);
+  points.position.set(0, 0, 0);
+  scene.add(points);
+}
+
+function updatePoints() {
+    positions = points.geometry.attributes.position.array;
+    colors = points.geometry.attributes.color.array;
+
+    for (let i = 0; i < width; i++) {
+        for (let j = 0; j < height; j++) {
+            const u = i / width;
+            const v = j / height;
+            const x = (u - 0.5);
+            const z = (v - 0.5);
+
+            const a = u - pointer.x;
+            const b = v - pointer.y;
+
+
+            const y = (Math.exp(-a*a - b * b)) * (Math.pow(a, 3) - 3 * a) * (Math.pow(b, 4) - 6 * Math.pow(b, 2) + 3) * Math.sin(a - t) * Math.cos(b - t) / 20;
+            positions[3 * k + 1] = y;
+
+            const intensity = ( y + 0.1 ) * 5;
+            colors[3 * k] = color.r * intensity;
+            colors[3 * k + 1] = color.g * intensity;
+            colors[3 * k + 2] = color.b * intensity;
+
+            k++;
+        }
+    }
 }
 
 
@@ -97,11 +130,7 @@ function render() {
     const r = Math.sin(t / (11 * Math.PI));
     const g = Math.cos(t / (3 * Math.PI));
     const b = Math.sin(t / (7 * Math.PI));
-    const points = generatePointCloud( new THREE.Color(r, g, b), width, height, t);
-    points.scale.set(5, 10, 10);
-    points.position.set(0, 0, 0);
-    scene.add(points);
-
+    updatePoints();
     renderer.render(scene, camera);
 }
 
