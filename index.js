d50a2bfa4a55d13c1f074b055cf8f12d37ef589f
Glenn LeBlanc <g.leblanc135@gmail.com>
2021-05-25 21:21:29 -0700

    Begin point cloud

--- index.js
+++ index.js
@@ -1,18 +1,71 @@
 import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
 
+
+function generatePointCloudGeometry( color, width, length ) {
+    const geometry = new THREE.BufferGeometry();
+    const numPoints = width * length;
+
+    const positions = new Float32Array( numPoints * 3 );
+    const colors = new Float32Array( numPoints * 3 );
+
+    let k = 0;
+
+    for (let i = 0; i < width; i++) {
+        for (let j = 0; j < height; j++) {
+            const u = i / width;
+            const v = j / length;
+            const x = u - 0.5;
+            const y = (Math.cos(u * Math.PI * 4) + Math.Sin( v * Math.PI * 8)) / 20;
+            const z = v - 0.5;
+
+            positions[3 * k] = x;
+            positions[3 * k + 1] = y;
+            positions[3 * k + 2] = z;
+
+            const intensity = ( y + 0.1 ) * 5;
+            colors[3 * k] = color.r * intensity;
+            colors[3 * k + 1] = color.g * intensity;
+            colors[3 * k + 2] = color.b * intensity;
+
+            k++;
+        }
+    }
+
+    geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+    geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
+    geometry.computeBoundingBox();
+
+    return geometry;
+}
+
+
+function generatePointCloud(color, width, length) {
+    const geometry = generatePointCloudGeometry(color, width, length);
+    const material = new THREE.PointsMaterial({size: pointSize, vertexColors: true});
+
+    return new THREE.Points(geometry, material);
+}
+
+
+function onPointerMove(event) {
+    pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+    pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+}
+
 function main() {
 
   const canvas = document.querySelector('#c');
-  const renderer = new THREE.WebGLRenderer({canvas});
 
-  const fov = 75;
-  const aspect = 2;  // the canvas default
-  const near = 0.1;
-  const far = 5;
+  const scene = new THREE.Scene();
+  const clock = new THREE.Clock();
+
+  const renderer = new THREE.WebGLRenderer({canvas});
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
   camera.position.z = 2;
 
-  const scene = new THREE.Scene();
+  document.addEventListener('pointermove', onPointerMove);
 
   const geometry = new THREE.BoxGeometry(1, 1, 1);
   const material = new THREE.MeshBasicMaterial({color: 0x44aa88});  // greenish blue
@@ -21,6 +74,7 @@ function main() {
   scene.add(cube);
 
   renderer.render(scene, camera);
+
 }
 
 main();
