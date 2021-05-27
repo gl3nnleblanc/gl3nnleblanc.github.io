fe3477017ec8ac4249027163844fc6e1d87e2c10
Glenn LeBlanc <g.leblanc135@gmail.com>
2021-05-26 17:08:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -122,10 +122,10 @@ function updatePoints() {
 
             const y = (Math.exp(-a*a / 2) * (Math.pow(a, 6) - 15 * Math.pow(a, 4) + 45 * Math.pow(a, 2) - 15)) *
                 (Math.exp(-b*b / 2) * (Math.pow(b, 6) - 15 * Math.pow(b, 4) + 45 * Math.pow(b, 2) - 15)) *
-                Math.sin(x - 1.5*t) * Math.sin(z - 1.5*t) / 300
+                Math.sin(x - 1.5*t) * Math.sin(z - 1.5*t) / 900
             positions[3 * k + 1] = y;
 
-            const intensity = Math.exp((-a*a - b*b) / 3)
+            const intensity = Math.tanh(y * y)
             colors[3 * k] = r * intensity;
             colors[3 * k + 1] = g * intensity;
             colors[3 * k + 2] = b * intensity;
