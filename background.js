3bc4d4f67dabb624e5b542908e5d5b13e7dded13
Glenn LeBlanc <g.leblanc135@gmail.com>
2021-06-14 18:06:29 -0700

    Modify point count

--- background.js
+++ background.js
@@ -24,8 +24,8 @@ const b = 0;
 let points;
 
 // Global dimensions
-const width = 350;
-const height = 350;
+const width = window.innerWidth / 5;
+const height = window.innerHeight / 5;
 
 // Projecting mouse coordinates onto 3D plane
 let projX;
