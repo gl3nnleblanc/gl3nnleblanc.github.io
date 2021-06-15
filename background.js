c8d760fd6d15ba0c6e2efad7ef9aa385baacece5
Glenn LeBlanc <g.leblanc135@gmail.com>
2021-06-14 18:16:09 -0700

    Decrease point count

--- background.js
+++ background.js
@@ -24,8 +24,8 @@ const b = 0;
 let points;
 
 // Global dimensions
-const width = window.innerWidth / 3;
-const height = window.innerHeight / 3;
+const width = window.innerWidth / 4;
+const height = window.innerHeight / 4;
 
 // Projecting mouse coordinates onto 3D plane
 let projX;
